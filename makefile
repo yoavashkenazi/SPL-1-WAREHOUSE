6d759d412adc5bb1d62296de7cfe73bcfb11b508
Yoav Ashkenazi <yoav.ash9@gmail.com>
2024-01-17 14:52:36 +0000

    a

--- makefile
+++ makefile
@@ -2,11 +2,15 @@
 all: clean compile link
 
 link:
-	g++ -o bin/hello bin/main.o bin/someclass.o
+	g++ -o bin/hello bin/main.o bin/WareHouse.o
 
 compile:
 	g++ -g -Wall -Weffc++ -c -o bin/main.o src/main.cpp
-	g++ -g -Wall -Weffc++ -c -o bin/someclass.o src/someclass.cpp
+	g++ -g -Wall -Weffc++ -c -o bin/WareHouse.o src/WareHouse.cpp
+	g++ -g -Wall -Weffc++ -c -o bin/Volunteer.o src/Volunteer.cpp
+	g++ -g -Wall -Weffc++ -c -o bin/Order.o src/Order.cpp
+	g++ -g -Wall -Weffc++ -c -o bin/Customer.o src/Customer.cpp
+	g++ -g -Wall -Weffc++ -c -o bin/BaseAction.o src/BaseAction.cpp
 
 clean:
 	rm -f bin/*
--- src/WareHouse.cpp
+++ src/WareHouse.cpp
@@ -1,18 +1,20 @@
-#include "WareHouse.h";
+#include "WareHouse.h"
 #include <iostream>
 #include <fstream>
 #include <string>
 
-WareHouse::WareHouse(const string &configFilePath) :
+WareHouse::WareHouse(const string &configFilePath)
 {
     // Open the text file
-    std::ifstream inputFile("example.txt");
+    std::ifstream inputFile(configFilePath);
+
+    start();
 
     // Check if the file is open
     if (!inputFile.is_open())
     {
         std::cerr << "Error opening file!" << std::endl;
-        return 1;
+        return;
     }
 
     // Read the file line by line
@@ -21,12 +23,8 @@ WareHouse::WareHouse(const string &configFilePath) :
     {
         // Process each line
         std::cout << "Line: " << line << std::endl;
-
-        // You can add your parsing logic here based on the content of each line
     }
 
     // Close the file
     inputFile.close();
-
-    return 0;
 }
