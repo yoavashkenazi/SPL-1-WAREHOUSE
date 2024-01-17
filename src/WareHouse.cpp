60da5b3ac951e49d2876cc061e90a9f08d54533e
Yoav Ashkenazi <yoav.ash9@gmail.com>
2024-01-17 15:58:36 +0000

    a

--- src/WareHouse.cpp
+++ src/WareHouse.cpp
@@ -19,8 +19,26 @@ WareHouse::WareHouse(const string &configFilePath)
     std::string line;
     while (std::getline(inputFile, line))
     {
+        char delimiter = '#';
+        std::vector<std::string> result;
+        size_t start = 0;
+        size_t end = line.find(delimiter);
+
+        while (end != std::string::npos)
+        {
+            result.push_back(line.substr(start, end - start));
+            start = end + 1;
+            end = line.find(delimiter, start);
+        }
+
+        // Add the last substring after the last delimiter
+        result.push_back(line.substr(start));
+
         // Process each line
-        std::cout << "Line: " << line << std::endl;
+        if (!result[0].empty())
+        {
+            std::cout << result[0] << std::endl;
+        }
     }
 
     // Close the file
