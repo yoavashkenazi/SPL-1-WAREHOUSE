3c3630392b4f9ec095328e734846d8e261448dc3
Yoav Ashkenazi <yoav.ash9@gmail.com>
2024-01-18 12:30:00 +0000

    x

--- src/WareHouse.cpp
+++ src/WareHouse.cpp
@@ -3,46 +3,9 @@
 #include <fstream>
 #include <string>
 
-WareHouse::WareHouse(const string &configFilePath)
+WareHouse::WareHouse(const string &configFilePath) : isOpen(false), customerCounter(0), volunteerCounter(0)
 {
-    // Open the text file
-    std::ifstream inputFile(configFilePath);
-
-    // Check if the file is open
-    if (!inputFile.is_open())
-    {
-        std::cerr << "Error opening file!" << std::endl;
-        return;
-    }
-
-    // Read the file line by line
-    std::string line;
-    while (std::getline(inputFile, line))
-    {
-        char delimiter = '#';
-        std::vector<std::string> result;
-        size_t start = 0;
-        size_t end = line.find(delimiter);
-
-        while (end != std::string::npos)
-        {
-            result.push_back(line.substr(start, end - start));
-            start = end + 1;
-            end = line.find(delimiter, start);
-        }
-
-        // Add the last substring after the last delimiter
-        result.push_back(line.substr(start));
-
-        // Process each line
-        if (!result[0].empty())
-        {
-            std::cout << result[0] << std::endl;
-        }
-    }
-
-    // Close the file
-    inputFile.close();
+    this->configFileProccessing(configFilePath)
 }
 
 void WareHouse::start()
@@ -117,4 +80,50 @@ void WareHouse::close()
 void WareHouse::open()
 {
     this->isOpen = true;
+}
+
+void WareHouse::AddCustomer(Customer *customer)
+{
+}
+
+void WareHouse::configFileProccessing(const string &configFilePath)
+{
+    // Open the text file
+    std::ifstream inputFile(configFilePath);
+
+    // Check if the file is open
+    if (!inputFile.is_open())
+    {
+        std::cerr << "Error opening file!" << std::endl;
+        return;
+    }
+
+    // Read the file line by line
+    std::string line;
+    while (std::getline(inputFile, line))
+    {
+        char delimiter = '#';
+        std::vector<std::string> result;
+        size_t start = 0;
+        size_t end = line.find(delimiter);
+
+        while (end != std::string::npos)
+        {
+            result.push_back(line.substr(start, end - start));
+            start = end + 1;
+            end = line.find(delimiter, start);
+        }
+
+        // Add the last substring after the last delimiter
+        result.push_back(line.substr(start));
+
+        // Process each line
+        if (!result[0].empty())
+        {
+            std::cout << result[0] << std::endl;
+        }
+    }
+
+    // Close the file
+    inputFile.close();
 }
