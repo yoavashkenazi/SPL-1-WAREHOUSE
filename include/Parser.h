4cb9caf25757ccbc142d9579a1d2b7f2fb934a43
yuvalanavi <yuvalanavi@gmail.com>
2024-01-20 10:26:55 +0000

    parser

---
+++ include/Parser.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include "../include/BaseAction.h"
+#include "../include/Volunteer.h"
+#include "../include/WareHouse.h"
