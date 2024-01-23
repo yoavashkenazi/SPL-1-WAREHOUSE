9f05a5ecde4ab90bf76b09aaca2578c53c248066
Yoav Ashkenazi <yoav.ash9@gmail.com>
2024-01-23 14:29:21 +0000

    fix

--- src/SimulateStep.cpp
+++ src/SimulateStep.cpp
@@ -2,17 +2,24 @@
 #include "../include/Volunteer.h"
 #include <iostream>
 
-SimulateStep::SimulateStep(int numOfSteps): BaseAction(), numOfSteps(numOfSteps) {
+SimulateStep::SimulateStep(int numOfSteps) : BaseAction(), numOfSteps(numOfSteps)
+{
 }
-void SimulateStep::act(WareHouse &wareHouse){
-    for (int i = 0; i < numOfSteps ; i++){ 
-        //part 1 - assigning pending orders to volunteers
+void SimulateStep::act(WareHouse &wareHouse)
+{
+    for (int i = 0; i < numOfSteps; i++)
+    {
+        // part 1 - assigning pending orders to volunteers
         vector<Order *> pendingOrdersVector = wareHouse.getOrders(0);
-        for (Order* o : pendingOrdersVector){//each order in pendingOrders
-            if (o->getStatus() == OrderStatus::PENDING){ //if the order status is pending, hand it to collector
+        for (Order *o : pendingOrdersVector)
+        { // each order in pendingOrders
+            if (o->getStatus() == OrderStatus::PENDING)
+            { // if the order status is pending, hand it to collector
                 vector<Volunteer *> volunteerVector = wareHouse.getVolunteers();
-                for (Volunteer* v : volunteerVector){ //searching for an available collector
-                    if ((CollectorVolunteer* collector = dynamic_cast<CollectorVolunteer*>(v)) && (v->canTakeOrder(*o))){
+                for (Volunteer *v : volunteerVector)
+                { // searching for an available collector
+                    if ((CollectorVolunteer *collector = dynamic_cast<CollectorVolunteer *>(v)) && (v->canTakeOrder(*o)))
+                    {
                         v->acceptOrder(*o);
                         wareHouse.moveOrderBetweenVectors(o->getId());
                         o->setCollectorId(v->getId());
@@ -20,10 +27,13 @@ void SimulateStep::act(WareHouse &wareHouse){
                     }
                 }
             }
-            else{//if the order status is collecting, hand it to a Driver
+            else
+            { // if the order status is collecting, hand it to a Driver
                 vector<Volunteer *> volunteerVector = wareHouse.getVolunteers();
-                for (Volunteer* v : volunteerVector){ //searching for an available driver
-                    if ((DriverVolunteer* driver = dynamic_cast<DriverVolunteer*>(v)) && (v->canTakeOrder(*o))){
+                for (Volunteer *v : volunteerVector)
+                { // searching for an available driver
+                    if ((DriverVolunteer *driver = dynamic_cast<DriverVolunteer *>(v)) && (v->canTakeOrder(*o)))
+                    {
                         v->acceptOrder(*o);
                         wareHouse.moveOrderBetweenVectors(o->getId());
                         o->setDriverId(v->getId());
@@ -32,41 +42,50 @@ void SimulateStep::act(WareHouse &wareHouse){
                 }
             }
         }
-        //part 2 - Decreasing timeLeft and DistanceLeft
+        // part 2 - Decreasing timeLeft and DistanceLeft
         vector<Volunteer *> volunteerVector = wareHouse.getVolunteers();
-        for (Volunteer* v : volunteerVector){
-            if (v->isBusy()){
+        for (Volunteer *v : volunteerVector)
+        {
+            if (v->isBusy())
+            {
                 v->step();
             }
         }
-        //part 3 - checking each inProccess order if it was completed and moving it to pending/completed
+        // part 3 - checking each inProccess order if it was completed and moving it to pending/completed
         vector<Order *> inProccessOrdersVector = wareHouse.getOrders(1);
-        for (Order* o : inProccessOrdersVector){
-            if ((wareHouse.getVolunteer(o->getCollectorId())).getCompletedOrderId()!=NO_ORDER){ //if the order proccessing is completed
-                if (o->getStatus() == OrderStatus::COLLECTING){
-                    wareHouse.moveOrderBetweenVectors(o->getId()); //move from inProcces to Pending
+        for (Order *o : inProccessOrdersVector)
+        {
+            if ((wareHouse.getVolunteer(o->getCollectorId())).getCompletedOrderId() != NO_ORDER)
+            { // if the order proccessing is completed
+                if (o->getStatus() == OrderStatus::COLLECTING)
+                {
+                    wareHouse.moveOrderBetweenVectors(o->getId()); // move from inProcces to Pending
                 }
-                else{
-                    wareHouse.moveOrderBetweenVectors(o->getId()); //move from inProccess to Completed
+                else
+                {
+                    wareHouse.moveOrderBetweenVectors(o->getId()); // move from inProccess to Completed
                     o->setStatus(OrderStatus::COMPLETED);
                 }
             }
         }
-        //part 4 - deleting each limited volunteer that reached his maxOrders
-        vector<Volunteer *> volunteerVector = wareHouse.getVolunteers();
-        for (Volunteer* v : volunteerVector){
-            if (!(v->hasOrdersLeft) && v->getActiveOrderId()==NO_ORDER){
+        // part 4 - deleting each limited volunteer that reached his maxOrders
+        for (Volunteer *v : volunteerVector)
+        {
+            if (!(v->hasOrdersLeft()) && v->getActiveOrderId() == NO_ORDER)
+            {
                 wareHouse.deleteLimitedVolunteer(v->getId());
             }
         }
     }
 }
-std::string SimulateStep::toString() const {
+std::string SimulateStep::toString() const
+{
     string output;
     output = "step " + std::to_string(numOfSteps) + " COMPLETED";
     return output;
 }
-SimulateStep *SimulateStep::clone() const {
+SimulateStep *SimulateStep::clone() const
+{
     SimulateStep *cloneSimulateStep = new SimulateStep(numOfSteps);
     if (this->getStatus() == ActionStatus::COMPLETED)
     {
